b0e5027994c4edbf474f3df63be2ca8945c6eb5f
Idris Wonuola <Idris.wonuola@wemabank.com>
2020-12-10 15:58:45 +0100

    spell checking the test typed

--- script.js
+++ script.js
@@ -5,49 +5,63 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 var timer = [0,0,0,0];
+var interval;
+var timerRqunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-function leadingZero(time){
-    if (time <= 9){
+function leadingZero(time) {
+    if (time <= 9) {
         time = "0" + time;
     }
-    return time
+    return time;
 }
 
 // Run a standard minute/second/hundredths timer:
-function runTimer(){
-    let currentTimer = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
-    theTimer.innerHTML = currentTimer;
+function runTimer() {
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    theTimer.innerHTML = currentTime;
     timer[3]++;
 
     timer[0] = Math.floor((timer[3]/100)/60);
-    timer[1] = Math.floor((timer[3]/100)- timer[0] * 60);
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
-
 // Match the text entered with the provided text on the page:
-function spellCheck(){
+function spellCheck() {
     let textEntered = testArea.value;
-    console.log(textEntered)
+    let originTextMatch = originText.substring(0,textEntered.length);
+
+     //here am checking if the text entered is equat to the main text am checking 
+    if (textEntered == originText) {
+        clearInterval(interval);
+        testWrapper.style.borderColor = "#429890";
+    } else {
+        if (textEntered == originTextMatch) { //here am checking if the character typed is equal to the character of the text 
+            testWrapper.style.borderColor = "#65CCf3";
+        } else {
+            testWrapper.style.borderColor = "#E95D0F";
+        }
+    }
+    
 }
 
 // Start the timer:
- function start(){
-     let textEnteredLength = testArea.value.length;
-     if (textEnteredLength === 0){
-         setInterval(runTimer, 10);
-     }
-     console.log(textEnteredLength)
- }
-
-// Reset everything:
-function reset(){
-    console.log("Reset button has been pressed!");
+function start() {
+    let textEnterdLength = testArea.value.length;
+    if (textEnterdLength === 0 && !timerRqunning) {
+        timerRqunning = true; //this help stop the timer when the test is cleared.
+        interval = setInterval(runTimer, 10);
+    }
+    console.log(textEnterdLength);
 }
 
+// Reset everything:
+function reset() {
+    console.log("reset button has been pressed!");
+}
 
-// Event listeners for keyboard input and the reset button:
-testArea.addEventListener("keypress",start,false);
-testArea.addEventListener("keyup",spellCheck,false);
-testArea.addEventListener("click",reset,false);
+// Event listeners for keyboard input and the reset
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
